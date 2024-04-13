5a6ed1a81967cd033687380338ae58b0c4929598
joacop01 <joaquin.palacio@ingenieria.uner.edu.ar>
2024-04-12 22:46:33 -0300

    Ejercicios 1 y 2 de la guia 2

--- firmware/projects/guia2_ej1/main/guia2_ej1.c
+++ firmware/projects/guia2_ej1/main/guia2_ej1.c
@@ -1,23 +1,26 @@
-/*! @mainpage Template
- *
+/*! @mainpage Guia2_ej1
+	Resolución del ejercicio 1 de la guía 2 de electrónica programable 1C2024
+ *	
  * @section genDesc General Description
- *
- * This section describes how the program works.
- *
- * <a href="https://drive.google.com/...">Operation Example</a>
- *
+ 	Mediante un sensor de ultrasonido se mide la distancia a un objeto particular, la cual se muestra por 
+ 	la pantalla LCD, asi como por la cantidad de leds que se encienden de acuerdo a la distancia medida
+ 	Se realiza mediante el uso de tareas
+ * 
  * @section hardConn Hardware Connection
  *
  * |    Peripheral  |   ESP32   	|
  * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
+ * | 	ECHO	 	| 	GPIO_3		|
+ * | 	TRIGGER	 	| 	GPIO_2		|
+ * | 	 +5V	 	| 	 +5V		|
+ * | 	 GND	 	| 	 GND		|
  *
  *
  * @section changelog Changelog
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | 12/09/2023 | Document creation		                         |
+ * | 12/04/2024 | Document creation		                         |
  *
  * @author Joaquin Palacio
  *
@@ -33,13 +36,37 @@
 #include "hc_sr04.h"
 #include "lcditse0803.h"
 #include "switch.h"
+#include "timer_mcu.h"
 
 /*==================[macros and definitions]=================================*/
+/**
+ * @def CONFIG_DELAY
+ * @brief Tiempo de delay de las tareas relacionadas al sensor, la pantalla LCD y los leds
+*/
 #define CONFIG_DELAY 1000
+
+/**
+ * @def CONFIG_DELAY_SWITCHES
+ * @brief Tiempo de delay relacionado con la lectura de los switches
+*/
 #define CONFIG_DELAY_SWITCHES 100
 
+/**
+ * @def distancia
+ * @brief distancia calculada por el sensor hc-rs04
+*/
 uint16_t distancia;
+
+/**
+ * @def on
+ * @brief booleano que indica si se realiza la medicion y acciones asociadas
+*/
 bool on = 0;
+
+/**
+ * @def hold
+ * @brief booleano que indica si se debe manetener la pantalla LCD congelada
+*/
 bool hold = 0;
 
 /*==================[internal data definition]===============================*/
@@ -47,7 +74,53 @@ TaskHandle_t calc_dist_task_handle = NULL;
 TaskHandle_t manejo_leds_task_handle = NULL;
 TaskHandle_t mostrar_dist_task_handle = NULL;
 TaskHandle_t lecuta_switches_task_handle = NULL;
+
+/**
+ * @fn static void CalcDsit
+ * @brief El sensor hc-sr04 calcula la distancia a un objeto
+ * @param[in] pvParameter
+ * @return 
+ */
+static void CalcDist(void *pvParameter);
+
+/**
+ * @fn static void ManejoLeds
+ * @brief Se encarga del control de los leds de acuerdo a la distancia medida
+ * @param[in] pvParameter
+ * @return 
+ */
+static void ManejoLeds(void *pvParameter);
+
+/**
+ * @fn static void MostrarDsit
+ * @brief Muestra el valor de la distancia medida en la pantalla LCD
+ * @param[in] pvParameter
+ * @return 
+ */
+static void MostrarDist(void *pvParameter);
+
+/**
+ * @fn static void LecturaSwitches
+ * @brief Se encarga de leer el estado de los switches y actualizar los booleanos de on y hold
+ * @param[in] pvParameter
+ * @return 
+ */
+static void LecturaSwitches(void *pvParameter);
+
 /*==================[internal functions declaration]=========================*/
+/**
+ * @fn void FuncTimerA
+ * @brief Notifica a las tareas de calcular distancia, manejo de leds y mostrar distancias para que se 
+ * encuentren listas
+ * @param[in] param
+ * @return 
+ */
+void FuncTimerA(void* param){
+    vTaskNotifyGiveFromISR(calc_dist_task_handle, pdFALSE);
+	vTaskNotifyGiveFromISR(manejo_leds_task_handle, pdFALSE);
+	vTaskNotifyGiveFromISR(mostrar_dist_task_handle, pdFALSE);
+}
+
 static void CalcDist(void *pvParameter){
     while(true){
 		if(on == 1)
@@ -145,6 +218,7 @@ static void LecturaSwitches(void *pvParameter){
     }
 }
 
+
 /*==================[external functions definition]==========================*/
 void app_main(void){
 	LedsInit();
@@ -152,10 +226,9 @@ void app_main(void){
 	LcdItsE0803Init();
 	SwitchesInit();
 
-    xTaskCreate(&CalcDist, "MedirDist", 512, NULL, 5, &calc_dist_task_handle);
+	xTaskCreate(&CalcDist, "MedirDist", 512, NULL, 5, &calc_dist_task_handle);
     xTaskCreate(&ManejoLeds, "ManejoLeds", 512, NULL, 4, &manejo_leds_task_handle);
     xTaskCreate(&MostrarDist, "MostrarDist", 512, NULL, 4, &mostrar_dist_task_handle);
 	xTaskCreate(&LecturaSwitches, "LecturaSwitches", 512, NULL, 5, &lecuta_switches_task_handle);
-
 }
 /*==================[end of file]============================================*/
--- firmware/projects/guia2_ej2/main/guia2_ej2.c
+++ firmware/projects/guia2_ej2/main/guia2_ej2.c
@@ -1,23 +1,26 @@
-/*! @mainpage Template
- *
+/*! @mainpage Guia2_ej2
+	Resolución del ejercicio 2 de la guía 2 de electrónica programable 1C2024
+ *	
  * @section genDesc General Description
- *
- * This section describes how the program works.
- *
- * <a href="https://drive.google.com/...">Operation Example</a>
- *
+ 	Mediante un sensor de ultrasonido se mide la distancia a un objeto particular, la cual se muestra por 
+ 	la pantalla LCD, asi como por la cantidad de leds que se encienden de acuerdo a la distancia medida
+ 	Se realiza mediante el uso de tareas e interrupciones
+ * 
  * @section hardConn Hardware Connection
  *
  * |    Peripheral  |   ESP32   	|
  * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
+ * | 	ECHO	 	| 	GPIO_3		|
+ * | 	TRIGGER	 	| 	GPIO_2		|
+ * | 	 +5V	 	| 	 +5V		|
+ * | 	 GND	 	| 	 GND		|
  *
  *
  * @section changelog Changelog
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | 12/09/2023 | Document creation		                         |
+ * | 12/04/2024 | Document creation		                         |
  *
  * @author Joaquin Palacio
  *
@@ -33,13 +36,31 @@
 #include "hc_sr04.h"
 #include "lcditse0803.h"
 #include "switch.h"
+#include "timer_mcu.h"
 
 /*==================[macros and definitions]=================================*/
-#define CONFIG_DELAY 1000
-#define CONFIG_DELAY_SWITCHES 100
+/**
+ * @def CONFIG_SENSOR_TIMER
+ * @brief Periodo del timerA
+*/
+#define CONFIG_SENSOR_TIMERA 1000000
 
+/**
+ * @def distancia
+ * @brief distancia calculada por el sensor hc-rs04
+*/
 uint16_t distancia;
+
+/**
+ * @def on
+ * @brief booleano que indica si se realiza la medicion y acciones asociadas
+*/
 bool on = 0;
+
+/**
+ * @def hold
+ * @brief booleano que indica si se debe manetener la pantalla LCD congelada
+*/
 bool hold = 0;
 
 /*==================[internal data definition]===============================*/
@@ -47,9 +68,57 @@ TaskHandle_t calc_dist_task_handle = NULL;
 TaskHandle_t manejo_leds_task_handle = NULL;
 TaskHandle_t mostrar_dist_task_handle = NULL;
 TaskHandle_t lecuta_switches_task_handle = NULL;
+
+/**
+ * @fn static void CalcDsit
+ * @brief El sensor hc-sr04 calcula la distancia a un objeto
+ * @param[in] pvParameter
+ * @return 
+ */
+static void CalcDist(void *pvParameter);
+
+/**
+ * @fn static void ManejoLeds
+ * @brief Se encarga del control de los leds de acuerdo a la distancia medida
+ * @param[in] pvParameter
+ * @return 
+ */
+static void ManejoLeds(void *pvParameter);
+
+/**
+ * @fn static void MostrarDsit
+ * @brief Muestra el valor de la distancia medida en la pantalla LCD
+ * @param[in] pvParameter
+ * @return 
+ */
+static void MostrarDist(void *pvParameter);
+
+/**
+ * @fn static void LecturaSwitch
+ * @brief Funcion que se llama al producirse una interrupcion por una de las teclas, modifica el valor de 
+ * on o de hold
+ * @param[in] tecla indica la tecla que ocasiono la interrupcion
+ * @return 
+ */
+static void LecturaSwitch(int *tecla);
+
 /*==================[internal functions declaration]=========================*/
+ /**
+ * @fn void FuncTimerA
+ * @brief Notifica a las tareas de calcular distancia, manejo de leds y mostrar distancias para que se 
+ * encuentren listas
+ * @param[in] param
+ * @return 
+ */
+void FuncTimerA(void* param){
+    vTaskNotifyGiveFromISR(calc_dist_task_handle, pdFALSE);
+	vTaskNotifyGiveFromISR(manejo_leds_task_handle, pdFALSE);
+	vTaskNotifyGiveFromISR(mostrar_dist_task_handle, pdFALSE);
+}
+
 static void CalcDist(void *pvParameter){
     while(true){
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    /* La tarea espera en este punto hasta recibir una notificación */
 		if(on == 1)
 		{
 			distancia = HcSr04ReadDistanceInCentimeters();
@@ -58,11 +127,13 @@ static void CalcDist(void *pvParameter){
 		{
 
 		}
-        vTaskDelay(CONFIG_DELAY / portTICK_PERIOD_MS);
+        //vTaskDelay(CONFIG_DELAY / portTICK_PERIOD_MS);
     }
 }
+
 static void ManejoLeds(void *pvParameter){
     while(true){
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    /* La tarea espera en este punto hasta recibir una notificación */
 		if(on == 1)
 		{
 			if(distancia < 10)
@@ -97,12 +168,12 @@ static void ManejoLeds(void *pvParameter){
 			LedOff(LED_2);
 			LedOff(LED_3);
 		}
-        vTaskDelay(CONFIG_DELAY / portTICK_PERIOD_MS);
+        //vTaskDelay(CONFIG_DELAY / portTICK_PERIOD_MS);
     }
 }
-
 static void MostrarDist(void *pvParameter){
     while(true){
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    /* La tarea espera en este punto hasta recibir una notificación */
 		if(on == 1)
 		{
 			if(hold == 1)
@@ -118,31 +189,21 @@ static void MostrarDist(void *pvParameter){
 		{
 			LcdItsE0803Off();
 		}
-		vTaskDelay(CONFIG_DELAY / portTICK_PERIOD_MS);
+		//vTaskDelay(CONFIG_DELAY / portTICK_PERIOD_MS);
     }
 }
 
-static void LecturaSwitches(void *pvParameter){
-    while(true){
-		uint8_t switches = SwitchesRead();
-		switch(switches)
-		{
-			case SWITCH_1:
-				on = !on;
-			break;
-			
-			case SWITCH_2:
-				hold = !hold;
-			break;
-
-			case SWITCH_1 | SWITCH_2:
-				on =! on;
-				hold =! hold;
-			break;
-		}
-		
-		vTaskDelay(CONFIG_DELAY_SWITCHES/ portTICK_PERIOD_MS);
-    }
+static void LecturaSwitch(int *tecla)
+{
+	switch (*tecla)
+	{
+	case SWITCH_1:
+	on = !on;
+		break;
+	case SWITCH_2:
+	hold = !hold;
+		break;
+	}
 }
 
 /*==================[external functions definition]==========================*/
@@ -152,10 +213,27 @@ void app_main(void){
 	LcdItsE0803Init();
 	SwitchesInit();
 
+	timer_config_t timer_sensor = {
+        .timer = TIMER_A,
+        .period = CONFIG_SENSOR_TIMERA,
+        .func_p = FuncTimerA,
+        .param_p = NULL
+    };
+
+	TimerInit(&timer_sensor);
+
+	SwitchActivInt(SWITCH_1, &LecturaSwitch, SWITCH_1 );
+	SwitchActivInt(SWITCH_2, &LecturaSwitch, SWITCH_2 );
+
     xTaskCreate(&CalcDist, "MedirDist", 512, NULL, 5, &calc_dist_task_handle);
     xTaskCreate(&ManejoLeds, "ManejoLeds", 512, NULL, 4, &manejo_leds_task_handle);
     xTaskCreate(&MostrarDist, "MostrarDist", 512, NULL, 4, &mostrar_dist_task_handle);
-	xTaskCreate(&LecturaSwitches, "LecturaSwitches", 512, NULL, 5, &lecuta_switches_task_handle);
+	//xTaskCreate(&LecturaSwitches, "LecturaSwitches", 512, NULL, 5, &lecuta_switches_task_handle);
+
+	TimerStart(timer_sensor.timer);
+
+
+    
 
 }
 /*==================[end of file]============================================*/
